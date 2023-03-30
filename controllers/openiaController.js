84a0b08af162bd62783ba3543c9be06fd54713fb
Gaurav1745 <103983412+Gaurav1745@users.noreply.github.com>
2023-03-30 06:38:56 -0700

    Update openiaController.js

--- controllers/openiaController.js
+++ controllers/openiaController.js
@@ -53,7 +53,7 @@ exports.chatbotController = async (req, res) => {
     const { text } = req.body;
     const { data } = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${text}`,
+      prompt: `what is${text}`,
 
 
       max_tokens: 300,
